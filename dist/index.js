339e4860c1b768e2973e47c707889d504b993895
Maxim Lobanov <v-malob@microsoft.com>
2020-08-09 09:17:42 +0300

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -1531,13 +1531,32 @@ module.exports = require("fs");
 
 "use strict";
 
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.SDKManager = void 0;
-const core_1 = __importDefault(__webpack_require__(470));
-const exec_1 = __importDefault(__webpack_require__(986));
+const core = __importStar(__webpack_require__(470));
+const exec = __importStar(__webpack_require__(986));
 const path_1 = __importDefault(__webpack_require__(622));
 class SDKManager {
     constructor() {
@@ -1556,11 +1575,11 @@ class SDKManager {
     async getPackageInfo(packageName) {
         let stdout = packageName;
         const stdoutListener = (data) => { stdout += data.toString(); };
-        const exitCode = await exec_1.default.exec(this.sdkManagerPath, ["--list"], { listeners: { stdout: stdoutListener } });
+        const exitCode = await exec.exec(this.sdkManagerPath, ["--list"], { listeners: { stdout: stdoutListener } });
         if (exitCode !== 0) {
             throw new Error(`'sdkmanager --list' has finished with exit code '${exitCode}'`);
         }
-        core_1.default.info(stdout);
+        core.info(stdout);
         return null;
     }
 }
